ed8cd3d602d9b2e8cff598afb86d566af6f52b64
JoseCarlos <jose.barbosa@estudante.ufla.br>
2020-09-30 22:00:15 -0300

    Implementando CRUD category

--- src/main/java/br/com/todo/resource/CategoryResource.java
+++ src/main/java/br/com/todo/resource/CategoryResource.java
@@ -4,10 +4,7 @@ import br.com.todo.entity.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import br.com.todo.service.CategoryService;
 
 import java.util.Optional;
@@ -25,5 +22,18 @@ public class CategoryResource {
             return ResponseEntity.ok(service.saveCategory(category.get()));
         }
         return ResponseEntity.noContent().build();
+    }
+    @GetMapping(value = "/get/all", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> getAllCategory() {
+        return ResponseEntity.ok(service.getAllCategory());
+    }
+
+    @DeleteMapping(value = "/delete", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Object> deleteCategory(@RequestParam Integer categoryId){
+        if (categoryId == null || categoryId == 0){
+            String erro = "Id nao Informado !!";
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(service.deleteCategory(categoryId));
     }
 }
--- src/main/java/br/com/todo/service/CategoryService.java
+++ src/main/java/br/com/todo/service/CategoryService.java
@@ -5,6 +5,8 @@ import br.com.todo.repository.CategoryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Service
@@ -21,5 +23,28 @@ public class CategoryService {
             return "ok, salvo !!";
         }
         return "Objeto category invalido.";
+    }
+
+    public List<Category> getAllCategory() {
+        Iterable<Category> categoryIterable = repository.findAll();
+        List<Category> categoryList = new ArrayList<>();
+        categoryIterable.forEach(categoryList::add);
+        return categoryList;
+    }
+
+    public  String deleteCategory(Integer CategoryId){
+        try{
+            repository.deleteById(CategoryId);
+            return "categoria deletada com sucesso!";
+        }catch (Exception e) {
+            return "Houve um erro com exclusão da categoria";
+        }
+
+    }
+
+    public  Category findByIdCategory(Integer categoryId){
+
+       return repository.findById(categoryId).isPresent() ?
+               repository.findById(categoryId).get() : new Category();
     }
 }
